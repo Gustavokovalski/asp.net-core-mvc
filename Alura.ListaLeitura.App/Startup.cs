789a7e4db9e8c5858a5b0cb3aaf56ceff7532471
Gustavokovalski <gustavokovalski.saporiti@gmail.com>
2019-05-24 08:02:15 -0300

    Roteamento, propriedade Request e codigo de retorno

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,8 +10,32 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
         }
+        public Task Roteamento(HttpContext context)
+        {   //
+            //Primeiro caminho -> /Livros/ParaLer
+            //Segundo caminho -> /Livros/Lendo
+            //Terceiro caminho -> /Livros/Lidos
+            //
+            var repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+
+                { "/Livros/ParaLer" , repo.ParaLer.ToString() },
+                { "/Livros/Lendo", repo.Lendo.ToString() },
+                { "/Livros/Lidos", repo.Lidos.ToString() }
+            };
+            ///
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+
+            context.Response.StatusCode = 404;
+            return context.Response.WriteAsync("Caminho inexistente");
+        }
+
 
         public Task LivrosParaLer(HttpContext context)
         {

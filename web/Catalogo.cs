e7373ea40518c1be3bbd8b59ab8367a9de7b052d
Gustavo <gustavokovalski.saporiti@gmail.com>
2019-05-29 14:52:24 -0300

    N/a

---
+++ web/Catalogo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public class Catalogo : ICatalogo
+    {
+        public List<Livro> GetLivros()
+        {
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Quem mexeu no meu teste ", 12.99m));
+            livros.Add(new Livro("002", "Fique rico com C# ", 22.99m));
+            livros.Add(new Livro("003", "Quem quer doce ", 1.99m));
+
+            return livros;
+        }
+    }
+}
--- web/Startup.cs
+++ web/Startup.cs
@@ -15,28 +15,25 @@ namespace web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICatalogo, Catalogo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
 
-                var livros = new List<Livro>();
-                livros.Add(new Livro("001", "Quem mexeu no meu teste", 12.99m));
-                livros.Add(new Livro("002", "Fique rico com C#", 22.99m));
-                livros.Add(new Livro("003", "Quem quer doce", 1.99m));
+            }
+
+            ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();
+            IRelatorio relatorio = new Relatorio(catalogo);
 
             app.Run(async (context) =>
             {
-                foreach (var livro in livros)
-                {
-                    await context.Response.WriteAsync($"{livro.Codigo}{livro.Nome}{livro.Preco}\r\n");
-                }
+                await relatorio.Imprimir(context);
             });
-            }
         }
     }
 }
